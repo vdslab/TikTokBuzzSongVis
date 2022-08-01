52cfb600f3df243c19a66a9a3bb9a0948d1d5e0f
Misato Watanabe <watamisa080513@gmail.com>
2022-08-02 03:25:39 +0900

    searchボタンの追加

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,12 +1,20 @@
 import * as React from "react";
-import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -15,12 +23,20 @@ export default function Header() {
           >
             <MenuIcon />
           </IconButton> */}
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            header title
-          </Typography>
-          {/* <Button color="inherit">Login</Button> */}
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              header title
+            </Typography>
+            {/* <Button color="inherit">Login</Button> */}
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <SearchIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
   );
 }

ce9ce0d993e5182b968508c2a51ef4d7f7e4485e
Misato Watanabe <watamisa080513@gmail.com>
2022-08-01 12:33:43 +0900

    本データで表示

--- components/charts/WordCloud.js
+++ components/charts/WordCloud.js
@@ -2,86 +2,6 @@ import * as d3 from "d3";
 import * as cloud from "d3-cloud";
 import { useMemo } from "react";
 
-const sampleData = [
-  { word: "ヤツ", score: 0.06382978723404255, negaposi: 0 },
-  { word: "君", score: 0.031914893617021274, negaposi: 0 },
-  { word: "分類", score: 0.031914893617021274, negaposi: 0 },
-  { word: "持っ", score: 0.031914893617021274, negaposi: 0 },
-  { word: "がる", score: 0.02127659574468085, negaposi: 0 },
-  { word: "習性", score: 0.02127659574468085, negaposi: 0 },
-  { word: "ない", score: 0.02127659574468085, negaposi: 0 },
-  { word: "キャ", score: 0.02127659574468085, negaposi: 0 },
-  { word: "それ", score: 0.02127659574468085, negaposi: 0 },
-  { word: "ん", score: 0.02127659574468085, negaposi: 0 },
-  { word: "ちっ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "区別", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ジャンル", score: 0.010638297872340425, negaposi: 0 },
-  { word: "分け", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ヒト", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ある", score: 0.010638297872340425, negaposi: 0 },
-  { word: "とか", score: 0.010638297872340425, negaposi: 0 },
-  { word: "世の中", score: 0.010638297872340425, negaposi: 0 },
-  { word: "2", score: 0.010638297872340425, negaposi: 0 },
-  { word: "種類", score: 0.010638297872340425, negaposi: -1.0 },
-  { word: "人間", score: 0.010638297872340425, negaposi: 0 },
-  { word: "いる", score: 0.010638297872340425, negaposi: 0 },
-  { word: "言う", score: 0.010638297872340425, negaposi: 0 },
-  { word: "君たち", score: 0.010638297872340425, negaposi: 0 },
-  { word: "標的", score: 0.010638297872340425, negaposi: 0 },
-  { word: "モテ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "やつ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "やる", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ヤッ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "陽", score: 0.010638297872340425, negaposi: 1.0 },
-  { word: "ら", score: 0.010638297872340425, negaposi: 0 },
-  { word: "落ち着か", score: 0.010638297872340425, negaposi: 1.0 },
-  { word: "気付か", score: 0.010638297872340425, negaposi: 0 },
-  { word: "本能", score: 0.010638297872340425, negaposi: 0 },
-  { word: "外側", score: 0.010638297872340425, negaposi: 0 },
-  { word: "覗い", score: 0.010638297872340425, negaposi: 0 },
-  { word: "いか", score: 0.010638297872340425, negaposi: 0 },
-  { word: "気分", score: 0.010638297872340425, negaposi: 0 },
-  { word: "乗ら", score: 0.010638297872340425, negaposi: 0 },
-  { word: "シンプル", score: 0.010638297872340425, negaposi: 0 },
-  { word: "曖昧", score: 0.010638297872340425, negaposi: -1.0 },
-  { word: "繊細", score: 0.010638297872340425, negaposi: 1.0 },
-  { word: "不明瞭", score: 0.010638297872340425, negaposi: -1.0 },
-  { word: "ナニカ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "出せ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "やっ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "イケ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "悟っ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ふり", score: 0.010638297872340425, negaposi: 0 },
-  { word: "スカ", score: 0.010638297872340425, negaposi: -1.0 },
-  { word: "うち", score: 0.010638297872340425, negaposi: 0 },
-  { word: "不安", score: 0.010638297872340425, negaposi: -1.0 },
-  { word: "なっ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ちゃっ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "する", score: 0.010638297872340425, negaposi: 0 },
-  { word: "アンタ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "ギフテッド", score: 0.010638297872340425, negaposi: 0 },
-  { word: "アタシ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "普通", score: 0.010638297872340425, negaposi: 0 },
-  { word: "主婦", score: 0.010638297872340425, negaposi: 0 },
-  { word: "良い", score: 0.010638297872340425, negaposi: 1.0 },
-  { word: "素晴らしい", score: 0.010638297872340425, negaposi: 1.0 },
-  { word: "不可能", score: 0.010638297872340425, negaposi: -1.0 },
-  { word: "証明", score: 0.010638297872340425, negaposi: 0 },
-  { word: "完成", score: 0.010638297872340425, negaposi: 1.0 },
-  { word: "夢", score: 0.010638297872340425, negaposi: 0 },
-  { word: "持て", score: 0.010638297872340425, negaposi: 0 },
-  { word: "言っ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "無責任", score: 0.010638297872340425, negaposi: 0 },
-  { word: "なり", score: 0.010638297872340425, negaposi: 0 },
-  { word: "喰わ", score: 0.010638297872340425, negaposi: 0 },
-  { word: "Habit", score: 0.010638297872340425, negaposi: 0 },
-  { word: "捨てる", score: 0.010638297872340425, negaposi: 0 },
-  { word: "度", score: 0.010638297872340425, negaposi: 0 },
-  { word: "見え", score: 0.010638297872340425, negaposi: 0 },
-  { word: "くる", score: 0.010638297872340425, negaposi: 0 },
-  { word: "価値", score: 0.010638297872340425, negaposi: 1.0 },
-];
-
 const legend = [
   { value: -1, text: "ネガティブ" },
   { value: 0, text: "ノーマル" },
@@ -89,32 +9,30 @@ const legend = [
 ];
 
 export default function WordCloudChart({ feature }) {
+  console.log(feature);
   const margin = {
     left: 5,
     right: 55,
     top: 5,
     bottom: 5,
   };
-  const contentWidth = 300;
-  const contentHeight = 200;
+  const contentWidth = 400;
+  const contentHeight = 400;
 
   const svgWidth = margin.left + margin.right + contentWidth;
   const svgHeight = margin.top + margin.bottom + contentHeight;
 
   const sizeScale = d3
     .scaleLinear()
-    .range([10, 100])
-    .domain([
-      sampleData[sampleData.length - 1]["score"],
-      sampleData[0]["score"],
-    ]);
+    .range([30, 90])
+    .domain([feature[feature.length - 1]["score"], feature[0]["score"]]);
 
   const colorScale = d3
     .scaleLinear()
     .domain([-1, 0, 1])
     .range(["#53befc", "#a3a3a3", "#fc77e8"]);
 
-  const cloudData = sampleData.map((data) => {
+  const cloudData = feature.map((data) => {
     return {
       word: data.word,
       size: sizeScale(data.score),

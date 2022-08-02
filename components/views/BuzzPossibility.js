2e027e18ed69bc18d6f249b9aa54c73a3cc72068
Misato Watanabe <watamisa080513@gmail.com>
2022-08-03 01:42:20 +0900

    スコアの取得と表示

---
+++ components/views/BuzzPossibility.js
@@ -0,0 +1,3 @@
+export default function BuzzPossibility({ songData }) {
+  return <div>バズり度{songData.rank}</div>;
+}
---
+++ pages/api/bebebe/song_buzz_score.js
@@ -0,0 +1,24 @@
+export default async function getSongBuzzScore(req, res) {
+  const id = JSON.parse(req.body);
+
+  // HACK:これでええんか？
+  const data = await fetch(
+    `${process.env.API_ENDPOINT}/song_buzz_score/${id}`,
+    {
+      method: "GET",
+      json: true,
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log("データの取得に失敗しました。", error);
+      return {};
+    });
+
+  res.status(200).json(data);
+}

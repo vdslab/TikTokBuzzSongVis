89a8882fa9b5abb2573c649a9bad875ce103972d
Misato Watanabe <watamisa080513@gmail.com>
2022-08-03 22:22:00 +0900

    類似曲の擬似データ表示

--- components/views/BuzzPossibility.js
+++ components/views/BuzzPossibility.js
@@ -1,3 +1,65 @@
+import { useEffect, useState } from "react";
+import { List, ListItem } from "@material-ui/core";
+import style from "./BuzzSongs.module.css";
+
 export default function BuzzPossibility({ songData }) {
-  return <div>バズり度</div>;
+  const [similarSongList, setSimilarSongList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const songReq = await fetch("/api/bebebe/similar_songs", {
+        method: "POST",
+        body: JSON.stringify(songData.id),
+      });
+      const data = await songReq.json();
+      setSimilarSongList(data);
+    })();
+  }, [songData]);
+
+  console.log(songData);
+
+  return (
+    <div>
+      <div>
+        {songData.title} バズり度 {songData.rank}
+        <audio controls src={songData.preview_url}></audio>
+      </div>
+      <div>
+        <div>{songData.title}の類似曲</div>
+        {similarSongList.map((song) => {
+          return (
+            <List key={song.id} className={style.songlist}>
+              <ListItem>
+                <div className={style.listitem}>
+                  {/* TODO:Imageタグに置き換える */}
+                  <div className={style.images_names}>
+                    <img
+                      src={song.album.images[0].url}
+                      style={{ width: "50px", height: "50px" }}
+                      alt=""
+                      className={style.image}
+                    ></img>
+                    <div className={style.names}>
+                      {/* HACK:Linkの方がいい？ */}
+                      <div
+                        onClick={() => {
+                          router.push(`/song/${song.id}`);
+                          if (props.clickAndClose) {
+                            props.handleClose();
+                          }
+                        }}
+                      >
+                        {song.name}
+                      </div>
+                      <div>{song.artists[0].name}</div>
+                    </div>
+                  </div>
+                  <audio controls src={song.preview_url}></audio>
+                </div>
+              </ListItem>
+            </List>
+          );
+        })}
+      </div>
+    </div>
+  );
 }

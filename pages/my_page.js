c53b415da1d10a10b44146612d7ee7967925b822
Misato Watanabe <watamisa080513@gmail.com>
2022-11-13 00:23:46 +0900

    PC, それ以外表示わけ

--- pages/my_page.js
+++ pages/my_page.js
@@ -3,14 +3,35 @@ import BookmarkList from "../components/views/BookmarkList";
 import SongDetail from "../components/views/SongDetail";
 import { useRecoilState } from "recoil";
 import { selectedSong } from "../components/atoms";
+import { useWindowSize } from "../components/hooks/getWindwSize";
+import { MINI_DISPLAY_SIZE } from "../components/common";
 
-export default function MyPage() {
+function MiniSizeDisplay() {
+  const [selectedSongId, setSelectedSongId] = useRecoilState(selectedSong);
+  return (
+    <div>
+      <Header />
+      <BookmarkList />
+    </div>
+  );
+}
+
+function DefaultSizeDisplay() {
   const [selectedSongId, setSelectedSongId] = useRecoilState(selectedSong);
   return (
     <div>
       <Header />
       <BookmarkList />
       <SongDetail showDate={true} key={selectedSongId} />
+    </div>
+  );
+}
+
+export default function MyPage() {
+  const { height, width } = useWindowSize();
+  return (
+    <div>
+      {width > MINI_DISPLAY_SIZE ? <DefaultSizeDisplay /> : <MiniSizeDisplay />}
     </div>
   );
 }

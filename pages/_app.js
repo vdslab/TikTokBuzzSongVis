7123ea4d62dbb42a0a62e1e16eeef0196d905a30
Misato Watanabe <watamisa080513@gmail.com>
2022-11-18 12:54:35 +0900

    add seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { RecoilRoot } from "recoil";
 import "../styles/global.css";
+import SEO from "../next-seo.config";
+import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }) {
   return (
     <RecoilRoot>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </RecoilRoot>
   );

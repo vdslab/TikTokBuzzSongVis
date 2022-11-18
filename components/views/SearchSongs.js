cc1832268585223260dadf00b94b8248a154e9f0
Misato Watanabe <watamisa080513@gmail.com>
2022-11-18 16:37:58 +0900

    fix

--- components/views/SearchSongs.js
+++ components/views/SearchSongs.js
@@ -42,7 +42,6 @@ export default function SearchSongs(props) {
         });
         const songListData = await songListRes.json();
         setSongList(songListData);
-        setSongList([]);
       }
     })();
   }, [inputSongName]);

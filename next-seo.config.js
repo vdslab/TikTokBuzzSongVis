7bc6ebd8d6ef67c1d2a0e137cb532a3fbd4ee8bd
Misato Watanabe <watamisa080513@gmail.com>
2023-01-24 17:01:54 +0900

    fix ogp

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,19 +1,26 @@
 export default {
   title: "BuzzLead",
   description:
-    " BuzzLeadは過去にTiktokで流行した曲のデータをもとに次にTikTokで流行する曲を予測するアプリケーションです。",
+    "BuzzLeadは過去にTiktokで流行した曲のデータをもとに次にTikTokで流行する曲を予測するアプリケーションです。",
   canonical: "https://buzzlead.vdslab.jp/",
   openGraph: {
     url: "https://buzzlead.vdslab.jp/",
     type: "website",
     locale: "ja_JP",
     site_name: "BuzzLead",
+    title: "BuzzLead",
+    description:
+      "BuzzLeadは過去にTiktokで流行した曲のデータをもとに次にTikTokで流行する曲を予測するアプリケーションです。",
     images: [
       {
         url: "https://buzzlead.vdslab.jp/images/ogp.png",
         width: 1200,
         height: 630,
+        type: "image/png",
       },
     ],
+  },
+  twitter: {
+    cardType: "summary_large_image",
   },
 };

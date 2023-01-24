444407ca62583373628028f6b9712e043098f9b1
Misato Watanabe <watamisa080513@gmail.com>
2023-01-24 17:22:43 +0900

    headeの設定

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { DefaultSeo } from "next-seo";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "../styles/nprogress.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   Router.events.on("routeChangeStart", () => NProgress.start());
@@ -13,6 +14,16 @@ export default function App({ Component, pageProps }) {
 
   return (
     <RecoilRoot>
+      <Head>
+        <title>BuzzLead</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="128x128"
+          href="https://buzzlead.vdslab.jp/images/icon.png"
+        />
+      </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </RecoilRoot>
